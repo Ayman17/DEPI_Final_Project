0465769e0720640d0e6dba5f9031e4b5c688e140
SALMAN <123901288+Ahmed-Ali-Salman@users.noreply.github.com>
2024-10-14 16:51:35 +0300

    Add files via upload
    merge between main and movies (under development)

---
+++ src/components/auth/auth_methods/SignInAll.jsx
@@ -0,0 +1,19 @@
+import SignInGoogle from './SignInGoogle';
+import React from 'react';
+import SignInTest from './SignInTest';
+
+const SignInAll = () => {
+  return (
+    <div
+      className="
+    ml-4
+    flex 
+    items-center
+    self-center
+    pt-2"
+    >
+    <SignInGoogle />
+    </div>
+  );
+};
+export default SignInAll;
