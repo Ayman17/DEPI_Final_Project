a0b5cfa05a7fcbe9c0843405f4a8f68e771a099f
Ayman17 <77356245+Ayman17@users.noreply.github.com>
2024-10-16 14:05:50 +0300

    Add tailwind.config to run the project will

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // This includes all React files in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
